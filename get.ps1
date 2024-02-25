664236328beeb90af515d3f85146297c162b81f7
Tremeschin <29046864+Tremeschin@users.noreply.github.com>
2024-02-24 22:19:16 -0400

    Reload path on get.ps1, print steps

--- get.ps1
+++ get.ps1
@@ -1,11 +1,17 @@
-# Install Python
+function Print-Step {
+    param([string]$step)
+    Write-Host ""
+    Write-Host > $step
+    Write-Host ""
+}
+
+Print-Step "Installing Git"
 winget install -e --id Git.Git --source winget
-
-# Install Git
+Print-Step "Installling Python"
 winget install -e --id Python.Python.3.11
-
-# Get BrokenSource repo
+print-Step "Reloading Path"
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
+print-Step "Cloning BrokenSource"
 git clone https://github.com/BrokenSource/BrokenSource
-
-# Run brakeit.py
+print-Step "Running brakeit.py"
 python ./BrokenSource/brakeit.py
